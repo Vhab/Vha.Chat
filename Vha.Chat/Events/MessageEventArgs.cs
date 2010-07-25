916dbbade91491ac7a6f1f7ec2e8d05dfcf51aca
Remco van Oosterhout <mail@vhabion.net>
2010-07-25 07:19:25 +0000

    - Fixed license headers

--- Vha.Chat/Events/MessageEventArgs.cs
+++ Vha.Chat/Events/MessageEventArgs.cs
@@ -1,6 +1,7 @@
 ﻿/*
 * Vha.Chat
 * Copyright (C) 2009-2010 Remco van Oosterhout
+* See Credits.txt for all aknowledgements.
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by

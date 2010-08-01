06300f53cf4788331c06553ac005a77e41ca91e3
Remco van Oosterhout <mail@vhabion.net>
2010-08-01 15:10:18 +0000

    - Implemented AomlFormatter

--- Vha.AOML/Formatting/AomlFormatter.cs
+++ Vha.AOML/Formatting/AomlFormatter.cs
@@ -22,6 +22,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Vha.AOML.DOM;
+using Vha.Common;
 
 namespace Vha.AOML.Formatting
 {
@@ -36,6 +37,17 @@ namespace Vha.AOML.Formatting
     public class AomlFormatter : Formatter
     {
         public readonly AomlFormatterStyle Style;
+        public string Quote
+        {
+            get
+            {
+                if (this.Style == AomlFormatterStyle.DoubleQuote)
+                    return "\"";
+                if (this.Style == AomlFormatterStyle.SingleQuote)
+                    return "'";
+                return null;
+            }
+        }
 
         public AomlFormatter()
         {
@@ -48,22 +60,102 @@ namespace Vha.AOML.Formatting
 
         public override string OnOpen() { return ""; }
         public override string OnClose() { return ""; }
-
         public override string OnBeforeElement(Element element) { return ""; }
         public override string OnAfterElement(Element element) { return ""; }
 
-        public override string OnAlignOpen(AlignElement element) { return ""; }
-        public override string OnAlignClose(AlignElement element) { return ""; }
-        public override string OnBreak(BreakElement element) { return ""; }
-        public override string OnContainerOpen(ContainerElement element) { return ""; }
-        public override string OnContainerClose(ContainerElement element) { return ""; }
-        public override string OnColorOpen(ColorElement element) { return ""; }
-        public override string OnColorClose(ColorElement element) { return ""; }
-        public override string OnImage(ImageElement element) { return ""; }
-        public override string OnLinkOpen(LinkElement element) { return ""; }
-        public override string OnLinkClose(LinkElement element) { return ""; }
-        public override string OnText(TextElement element) { return ""; }
-        public override string OnUnderlineOpen(UnderlineElement element) { return ""; }
-        public override string OnUnderlineClose(UnderlineElement element) { return ""; }
+        public override string OnAlignOpen(AlignElement element)
+        {
+            if (element.Alignment == Alignment.Inherit)
+                return "<div>";
+            return string.Format("<div align={0}{1}{0}",
+                this.Quote, element.Alignment.ToString().ToLower());
+        }
+
+        public override string OnAlignClose(AlignElement element)
+        {
+            return "</div>";
+        }
+
+        public override string OnBreak(BreakElement element)
+        {
+            return "<br>";
+        }
+
+        public override string OnContainerOpen(ContainerElement element)
+        {
+            return "";
+        }
+
+        public override string OnContainerClose(ContainerElement element)
+        {
+            return "";
+        }
+
+        public override string OnColorOpen(ColorElement element)
+        {
+            return string.Format("<font color={0}#{1:X2}{2:X2}{3:X2}{0}>",
+                this.Quote, element.Color.Red, element.Color.Green, element.Color.Blue);
+        }
+
+        public override string OnColorClose(ColorElement element)
+        {
+            return "</font>";
+        }
+
+        public override string OnImage(ImageElement element)
+        {
+            return string.Format("<img src={0}{1}://{2}{0}>",
+                this.Quote, element.ImageType.ToString(), element.Image);
+        }
+
+        public override string OnLinkOpen(LinkElement element)
+        {
+            string href = "";
+            string style = "";
+            switch (element.Link.Type)
+            {
+                case LinkType.Command:
+                    CommandLink command = (CommandLink)element.Link;
+                    href = "chatcmd://" + command.Command;
+                    break;
+                case LinkType.Element:
+                    ElementLink link = (ElementLink)element.Link;
+                    Formatter f = null;
+                    if (this.Style == AomlFormatterStyle.DoubleQuote)
+                        f = new AomlFormatter(AomlFormatterStyle.SingleQuote);
+                    else f = new PlainTextFormatter();
+                    href = "text://" + f.Format(link.Element);
+                    break;
+                case LinkType.Item:
+                    ItemLink item = (ItemLink)element.Link;
+                    href = string.Format("itemref://{0}/{1}/{2}",
+                        item.LowID, item.HighID, item.Quality);
+                    break;
+            }
+            if (!element.Stylized)
+                style = string.Format("{0}text-decoration:none{0}", this.Quote);
+            return string.Format("<a href={0}{1}{0}{2}>",
+                this.Quote, href, style);
+        }
+
+        public override string OnLinkClose(LinkElement element)
+        {
+            return "</a>";
+        }
+
+        public override string OnText(TextElement element)
+        {
+            return HTML.EscapeString(element.Text);
+        }
+
+        public override string OnUnderlineOpen(UnderlineElement element)
+        {
+            return "<u>";
+        }
+
+        public override string OnUnderlineClose(UnderlineElement element)
+        {
+            return "</u>";
+        }
     }
 }

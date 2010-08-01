4e2ade7e7f21baa752d2394ba8d285d61aee126c
Remco van Oosterhout <mail@vhabion.net>
2010-08-01 15:10:18 +0000

    - Implemented AomlFormatter

--- Vha.AOML/Program.cs
+++ Vha.AOML/Program.cs
@@ -14,8 +14,7 @@ namespace Vha.AOML
         static void Main(string[] args)
         {
             string data = "<test /><noes><a href=\"charref://123/0/<font color='CCInfoHeader'><font color = 'red'>888<font color=blue>999<center>123<right>3\"></font>4</font>5<div /></div>6</center><br></br>< img src=\"123\" />678";
-            //Parse(data);
-            //Dominize(data);
+            Dominize(data);
 
             data = "<font color=#79CBE6>Your query <font color=#FFFFFF>combined</font> has returned <font color=#FFFFFF>65</font> results\n" +
                 "  <font color=#79CBE6>Blackmane's Combined Officer's Headwear <font color=#CCCCCC>[</font><a href='itemref://257383/257383/300'>300</a><font color=#CCCCCC>] </font></font>\n" +
@@ -23,6 +22,8 @@ namespace Vha.AOML
                 "  <font color=#79CBE6>Combined Bio Samples - Stage One <font color=#CCCCCC>[</font><a href='itemref://267774/267774/250'>250</a><font color=#CCCCCC>] </font></font>\n" +
                 "  <font color=#79CBE6>Combined Bio Samples - Stage Three <font color=#CCCCCC>[</font><a href='itemref://267776/267776/250'>250</a><font color=#CCCCCC>] </font></font>\n" +
                 "  <font color=#79CBE6>Combined Bio Samples - Stage Two <font color=#CCCCCC>[</font><a href='itemref://267775/267775/250'>250</a><font color=#CCCCCC>] </font></font>\n";
+            Dominize(data);
+
             data = "<font color=#79CBE6>More Results »» <a href=\"text://<font color=#FFFFFF><font color=#3C8799>:::::::::::</font> VhaBot Client Terminal <font color=#3C8799>:::::::::::</font>\n" +
 "<font color=#3C8799>« </font><a href='chatcmd:///tell Itemsbot version' style='text-decoration:none'>About</a><font color=#3C8799> »     « </font><a href='chatcmd:///tell Itemsbot help' style='text-decoration:none'>Help</a><font color=#3C8799> »     « </font><a href='chatcmd:///close InfoView' style='text-decoration:none'>Close Terminal</a><font color=#3C8799> »</font>\n" +
 "</font><font color=#3C8799>¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯</font>\n" +
@@ -44,8 +45,9 @@ namespace Vha.AOML
 "<font color=#79CBE6>Combined Sharpshooter's Sleeves <font color=#CCCCCC>[</font><a href='itemref://246697/246698/1'>1</a><font color=#CCCCCC>] </font><font color=#CCCCCC>[</font><a href='itemref://246697/246698/300'>300</a><font color=#CCCCCC>]</font></font>\n" +
 "<img src=rdb://256301>\n" +
 "\">Click to View</a>";
-            data = "[Clan OOC] Flikfak: <a href=\"text://  <img src=rdb://214739> <font color=#FF3300>My Tradeskill Service</font>  <img src=rdb://214739><br> <br>Mech. Engi     --> 2100<br>Elec. Engi      --> 2100<br>Quantum FT   --> 2100<br>Weapon Smt  --> 2100<br>Pharma Tech  --> 1950</font><br>Nano Program --> 1800<br>Comp. Liter    --> 1800<br>Psychology    --> 1650<br>Chemistry      --> 2100<br>Tutoring        --> 1600<br>Break&Entry   --> 1250<br><br>All Tredaskills without city<br><br><a href='chatcmd:///tell Flikfak Master, i need your help>Tell Flik</a> or get <moo href='chatcmd:///fxscript EP03_Orbital_Attack #\n /fxscript EP03_Bomber_Event_01 #\n /text Boom>Nuked!</a>\">Need a Builder?</a> Engi @ Missi Term Nord</a>";
-            //Parse(data);
+            Dominize(data);
+
+            data = "[Clan OOC] Flikfak: <a href=\"text://  <img src=rdb://214739> <font color=#FF3300>My Tradeskill Service</font>  <img src=rdb://214739><br> <br>Mech. Engi     --> 2100<br>Elec. Engi      --> 2100<br>Quantum FT   --> 2100<br>Weapon Smt  --> 2100<br>Pharma Tech  --> 1950</font><br>Nano Program --> 1800<br>Comp. Liter    --> 1800<br>Psychology    --> 1650<br>Chemistry      --> 2100<br>Tutoring        --> 1600<br>Break&Entry   --> 1250<br><br>All Tredaskills without city<br><br><a href='chatcmd:///tell Flikfak Master, i need your help>Tell Flik</a> or get <a href='chatcmd:///fxscript EP03_Orbital_Attack #\n /fxscript EP03_Bomber_Event_01 #\n /text Boom>Nuked!</a>\">Need a Builder?</a> Engi @ Missi Term Nord</a>";
             Dominize(data);
 
             Console.ReadLine();
@@ -100,8 +102,9 @@ namespace Vha.AOML
         {
             Dominizer d = new Dominizer();
             Element e = d.Parse(aoml);
-            Formatter f = new PlainTextFormatter();
+            Formatter f = new AomlFormatter();
             Console.WriteLine(f.Format(e));
+            Console.WriteLine();
         }
     }
 }

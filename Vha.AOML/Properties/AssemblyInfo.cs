349881db2490c03a3bf0f29f49b6e522e9fea8d4
Remco van Oosterhout <mail@vhabion.net>
2010-08-28 14:28:34 +0000

    - Fixed up the assembly information a bit (including a typo :'()

--- Vha.AOML/Properties/AssemblyInfo.cs
+++ Vha.AOML/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vha.AOML")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for parsing and converting Anarchy Online's Markup Language (AOML)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Vha.Chat Development Team")]
 [assembly: AssemblyProduct("Vha.AOML")]
 [assembly: AssemblyCopyright("Copyright ©  2010 Remco van Oosterhout")]
 [assembly: AssemblyTrademark("")]

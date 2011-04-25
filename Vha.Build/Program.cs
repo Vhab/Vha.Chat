7e6ff70180bc2be46e8aca104a1a2f4d9911f1a5
Remco van Oosterhout <mail@vhabion.net>
2011-04-25 16:30:05 +0200

    - Added support for generating source files containing mercurial revision information based on a template file

--- Vha.Build/Program.cs
+++ Vha.Build/Program.cs
@@ -34,103 +34,17 @@ namespace Vha.Build
     {
         static void Main(string[] args)
         {
-            // Exit on DEBUG builds
-#if DEBUG
-            Console.WriteLine("Vha.Build is not available on debug builds");
-            Environment.Exit(0);
-#endif
+            // Check arguments
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments supplied");
+                Environment.Exit(1);
+            }
 
             // Run Vha.Build
             try
             {
-                // Parse arguments
-                Arguments arguments = new Arguments(args);
-
-                // Display help
-                if (arguments["help"] != null)
-                {
-                    Console.WriteLine("Vha.Build is a utility for post-build events.");
-                    Console.WriteLine("Currently its only feature is executing a NSIS script.");
-                    Console.WriteLine();
-                    Console.WriteLine("Available arguments:");
-                    Console.WriteLine("  -help");
-                    Console.WriteLine("    Optional. Displays this output");
-                    Console.WriteLine();
-                    Console.WriteLine("  -workingDirectory=\"...\"");
-                    Console.WriteLine("    Optional. Sets the working directory to start the installer script in");
-                    Console.WriteLine();
-                    Console.WriteLine("  -nsisScript=\"...\"");
-                    Console.WriteLine("    Mandatory. The .nsi script that should be run");
-                    Console.WriteLine();
-                    Console.WriteLine("  -assembly=\"...\"");
-                    Console.WriteLine("    Optional. Gathers information from a specific binary and feeds it to the .nsi script");
-                    Console.WriteLine();
-                    Console.WriteLine("  -nsisExecutable=\"...\"");
-                    Console.WriteLine("    Optional. Defines the NSIS executable that needs to be run (only works if nsisPath is also defined)");
-                    Console.WriteLine();
-                    Console.WriteLine("  -nsisPath=\"...\"");
-                    Console.WriteLine("    Optional. Defines the path NSIS executable resides in");
-                    Environment.Exit(0);
-                }
-
-                // - workingDirectory
-                string workingDirectory = Environment.CurrentDirectory;
-                if (arguments["workingDirectory"] != null)
-                    workingDirectory = arguments["workingDirectory"];
-                // - nsisScript
-                string nsisScript = "";
-                if (arguments["nsisScript"] != null)
-                    nsisScript = arguments["nsisScript"];
-                // - assembly
-                string assembly = "";
-                if (arguments["assembly"] != null)
-                    assembly = arguments["assembly"];
-
-                // Create NSIS tool
-                NSIS installer = null;
-                if (arguments["nsisExecutable"] != null && arguments["nsisPath"] != null)
-                {
-                    installer = new NSIS(arguments["nsisPath"], arguments["nsisExecutable"]);
-                }
-                else
-                {
-                    installer = new NSIS();
-                }
-
-                // Check if NSIS is installed
-                if (!installer.IsInstalled)
-                {
-                    Console.WriteLine("NSIS is not installed, will not create an installer!");
-                    Environment.Exit(0);
-                }
-
-                // Check if a script is defined
-                if (string.IsNullOrEmpty(nsisScript))
-                {
-                    Console.WriteLine("Missing mandatory argument 'nsisScript'");
-                    Environment.Exit(4);
-                }
-
-                // Gather assembly information
-                if (!string.IsNullOrEmpty(assembly))
-                {
-                    if (!Path.IsPathRooted(assembly))
-                        assembly = Path.Combine(workingDirectory, assembly);
-                    Assembly ass = Assembly.LoadFrom(assembly);
-                    installer.Define("ASSEMBLY_TITLE",
-                        ((AssemblyTitleAttribute)(ass.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0])).Title);
-                    installer.Define("ASSEMBLY_DESCRIPTION",
-                        ((AssemblyDescriptionAttribute)(ass.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false)[0])).Description);
-                    installer.Define("ASSEMBLY_COMPANY",
-                        ((AssemblyCompanyAttribute)(ass.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false)[0])).Company);
-                    installer.Define("ASSEMBLY_COPYRIGHT",
-                        ((AssemblyCopyrightAttribute)(ass.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0])).Copyright);
-                    installer.Define("ASSEMBLY_VERSION", ass.GetName().Version.ToString());
-                }
-
-                // Run installer script
-                bool success = installer.Run(workingDirectory, nsisScript);
-                if (!success) Environment.Exit(2);
+                Run(args);
             }
             catch (Exception ex)
             {
@@ -138,6 +52,183 @@ namespace Vha.Build
                 Environment.Exit(1);
             }
 
+        }
+
+        static void Run(string[] args)
+        {
+            // Parse arguments
+            Arguments arguments = new Arguments(args);
+
+            // - workingDirectory
+            string workingDirectory = Environment.CurrentDirectory;
+            if (arguments["workingDirectory"] != null)
+                workingDirectory = arguments["workingDirectory"];
+
+            // Switch between modes
+            int returnCode = 0;
+            switch (args[0])
+            {
+                case "help":
+                    Console.WriteLine("Vha.Build is a utility for pre and/or post-build events.");
+                    Console.WriteLine("Currently it features 2 modes, installer and temple");
+                    Console.WriteLine();
+                    Console.WriteLine("Available options:");
+                    Console.WriteLine("* help");
+                    Console.WriteLine("   Displays this output");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("* installer");
+                    Console.WriteLine("   Initializes and runs a NSIS script");
+                    Console.WriteLine();
+                    Console.WriteLine("   Available arguments:");
+                    Console.WriteLine();
+                    Console.WriteLine("    -nsisScript=\"...\"");
+                    Console.WriteLine("      Mandatory. The .nsi script that should be run");
+                    Console.WriteLine();
+                    Console.WriteLine("    -assembly=\"...\"");
+                    Console.WriteLine("      Optional. Gathers information from a specific binary and feeds it to the .nsi script");
+                    Console.WriteLine();
+                    Console.WriteLine("    -nsisExecutable=\"...\"");
+                    Console.WriteLine("      Optional. Defines the NSIS executable that needs to be run (only works if nsisPath is also defined)");
+                    Console.WriteLine();
+                    Console.WriteLine("    -nsisPath=\"...\"");
+                    Console.WriteLine("      Optional. Defines the path NSIS executable resides in");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("* template");
+                    Console.WriteLine("   Generates an source file based on a template input file");
+                    Console.WriteLine();
+                    Console.WriteLine("   Available arguments:");
+                    Console.WriteLine();
+                    Console.WriteLine("    -inputFile=\"...\"");
+                    Console.WriteLine("      Mandatory. The input template file to be parsed");
+                    Console.WriteLine();
+                    Console.WriteLine("    -outputFile=\"...\"");
+                    Console.WriteLine("      Mandatory. The output file to be written.");
+                    Console.WriteLine("      Will overwrite any existing file.");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Global arguments:");
+                    Console.WriteLine();
+                    Console.WriteLine("  -workingDirectory=\"...\"");
+                    Console.WriteLine("    Optional. Sets the working directory to start the finding and executing files in");
+                    Console.WriteLine();
+                    returnCode = 0;
+                    break;
+                case "installer":
+#if DEBUG
+                    Console.WriteLine("Running in debug mode, skipping installer generation");
+                    returnCode = 0;
+#else
+                    returnCode = RunInstaller(workingDirectory, arguments);
+#endif
+                    break;
+                case "template":
+                    returnCode = RunTemplate(workingDirectory, arguments);
+                    break;
+            }
+
+            // Exit
+            if (returnCode != 0)
+            {
+                Console.WriteLine("Vha.Build exiting with error code " + returnCode);
+            }
+            Environment.Exit(returnCode);
+        }
+
+        static int RunInstaller(string workingDirectory, Arguments args)
+        {
+            // - nsisScript
+            string nsisScript = "";
+            if (args["nsisScript"] != null)
+                nsisScript = args["nsisScript"];
+            // - assembly
+            string assembly = "";
+            if (args["assembly"] != null)
+                assembly = args["assembly"];
+
+            // Create NSIS tool
+            NSIS installer = null;
+            if (args["nsisExecutable"] != null && args["nsisPath"] != null)
+            {
+                installer = new NSIS(args["nsisPath"], args["nsisExecutable"]);
+            }
+            else
+            {
+                installer = new NSIS();
+            }
+
+            // Check if NSIS is installed
+            if (!installer.IsInstalled)
+            {
+                Console.WriteLine("NSIS is not installed, will not create an installer!");
+                Environment.Exit(0);
+            }
+
+            // Check if a script is defined
+            if (string.IsNullOrEmpty(nsisScript))
+            {
+                Console.WriteLine("Missing mandatory argument 'nsisScript'");
+                Environment.Exit(4);
+            }
+
+            // Gather assembly information
+            if (!string.IsNullOrEmpty(assembly))
+            {
+                if (!Path.IsPathRooted(assembly))
+                    assembly = Path.Combine(workingDirectory, assembly);
+                Assembly ass = Assembly.LoadFrom(assembly);
+                installer.Define("ASSEMBLY_TITLE",
+                    ((AssemblyTitleAttribute)(ass.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0])).Title);
+                installer.Define("ASSEMBLY_DESCRIPTION",
+                    ((AssemblyDescriptionAttribute)(ass.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false)[0])).Description);
+                installer.Define("ASSEMBLY_COMPANY",
+                    ((AssemblyCompanyAttribute)(ass.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false)[0])).Company);
+                installer.Define("ASSEMBLY_COPYRIGHT",
+                    ((AssemblyCopyrightAttribute)(ass.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0])).Copyright);
+                installer.Define("ASSEMBLY_VERSION", ass.GetName().Version.ToString());
+            }
+
+            // Run installer script
+            bool success = installer.Run(workingDirectory, nsisScript);
+            if (!success) return 2;
+            return 0;
+        }
+
+        static int RunTemplate(string workingDirectory, Arguments args)
+        {
+            // Input file
+            string inputFile = args["inputFile"];
+            if (string.IsNullOrEmpty(inputFile))
+            {
+                Console.WriteLine("Missing 'inputFile' argument");
+                return 1;
+            }
+            string inputFilePath = inputFile;
+            if (!Path.IsPathRooted(inputFile))
+                inputFilePath = Path.Combine(workingDirectory, inputFile);
+            if (!File.Exists(inputFile))
+            {
+                Console.WriteLine("Unable to locate '" + inputFile + "'");
+                return 2;
+            }
+
+            // Output file
+            string outputFile = args["outputFile"];
+            if (string.IsNullOrEmpty(outputFile))
+            {
+                Console.WriteLine("Missing 'outputFile' argument");
+                return 1;
+            }
+            string outputFilePath = outputFile;
+            if (!Path.IsPathRooted(outputFile))
+                outputFilePath = Path.Combine(workingDirectory, outputFile);
+
+            // Setup
+            Template template = new Template(workingDirectory, inputFilePath, outputFilePath);
+            bool success = template.Run();
+            if (!success) return 4;
+            return 0;
         }
     }
 }

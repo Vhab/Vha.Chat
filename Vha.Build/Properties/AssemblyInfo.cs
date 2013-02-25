156215c3f387d5658dd4d2a378c9d8fc282c4a74
Remco van Oosterhout <mail@vhabion.net>
2013-02-25 17:50:19 -0500

    - Updated copyright notice in assemblies

--- Vha.Build/Properties/AssemblyInfo.cs
+++ Vha.Build/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Vha.Build")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright © 2010-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

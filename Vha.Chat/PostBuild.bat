91a93b81794a70acb263f3442fbd6fd7e7a5d5e8
Remco van Oosterhout <remco@rvo.io>
2019-04-28 15:53:38 +0200

    First pass VS2017 conversion
    - Target .net standard 2.0 in libraries
    - Target .net framework 4.7.2 in client
    - Removed Vha.Build
    - Converted to new .csproj format

--- Vha.Chat/PostBuild.bat
+++
@@ -1,2 +0,0 @@
-cd /D %~1
-"../Vha.Build/bin/%2/Vha.Build.exe" installer -nsisScript=Installer.nsi -assembly=bin/%2/Vha.Chat.exe
--- Vha.Chat/PreBuild.bat
+++
@@ -1,2 +0,0 @@
-cd /D %~1
-"../Vha.Build/bin/%2/Vha.Build.exe" template -inputFile="Properties/AssemblyInfo.Template.cs" -outputFile="Properties/AssemblyInfo.cs"
